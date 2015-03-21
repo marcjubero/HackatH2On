17ee8d192bd4e70e466cbb4eca3fa901c409334b
marcjubero <wrrzag@gmail.com>
2015-03-21 13:22:55 +0100

    gitigore, node base structure

---
+++ backend/app.js
@@ -0,0 +1,15 @@
+var express = require('express'),
+    bodyParser = require('body-parser'),
+    app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 8080;
+
+
+require('./router/router')(app);
+
+app.listen(port, function() {
+	console.log("Magic happens on port " + port);	
+});
